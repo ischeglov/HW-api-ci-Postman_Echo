a2383e6788b78cfc7f97b7cdc756427b1fb0c798
imshcheglov <Ivan_mix@mail.ru>
2024-06-21 20:46:01 +0500

    Add gradle.yml and test

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,27 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    public void shouldPostText() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Task №3: Postman Echo") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Task №3: Postman Echo"));
+        ;
+    }
+}
